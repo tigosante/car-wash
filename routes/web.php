7b24179e3d35639f7817d4d6ec250b33db33e7ba
tigosante <tsilvasantos38@gmail.com>
2020-11-11 01:00:59 -0300

    integracao inical de auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::middleware("auth")->group(function () {
+    Route::get('/', [App\Http\Controllers\DashBoard\DashBoardController::class, 'index'])->name('dashBoard');
 });
